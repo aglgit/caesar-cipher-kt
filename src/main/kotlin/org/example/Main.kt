125285ff5692e528cc06f04bda9be25d83f10c15
Andreas L <aglgit@users.noreply.github.com>
2025-01-07 21:08:53 +0100

    Complete CLI with Clikt

--- src/main/kotlin/org/example/Main.kt
+++ src/main/kotlin/org/example/Main.kt
@@ -1,21 +1,34 @@
 package org.example
 
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.types.int
 import java.io.File
-
-// TODO: Complete CLI using Clikt
-// https://github.com/ajalt/clikt/tree/master
-// read in filename, cipher, decode flag
-fun main() {
-    val text = readFile("input/input.txt")
-    println("Text: \n$text")
-    val encoder = CaesarCipher(3)
-    val encodedText = encoder.encode(text)
-    val decodedText = encoder.decode(encodedText)
-    println("Encoded text: \n$encodedText")
-    println("Decoded text: \n$decodedText")
-}
 
 fun readFile(filename: String): String = File(filename)
     .inputStream()
     .readBytes()
     .toString(Charsets.UTF_8)
+
+class Cli : CliktCommand() {
+    val filename by option("-f", "--filename", help = "Path to input file)").required()
+    val cipher by option("-c", "--cipher", help = "Cipher to shift characters by").int().required()
+    val decode by option("-d", "--decode", help = "Whether to decode").flag(default = false)
+
+    override fun run() {
+        val text = readFile(filename)
+        val encoder = CaesarCipher(cipher)
+        if (!decode) {
+            val enc = encoder.encode(text)
+            println(enc)
+        } else {
+            val dec = encoder.decode(text)
+            println(dec)
+        }
+    }
+}
+
+fun main(args: Array<String>) = Cli().main(args)
